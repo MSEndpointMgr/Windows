582fcd70f0f13c17a0dfcae9452c3a59be93ac2f
Ben Whitmore <44303264+byteben@users.noreply.github.com>
2022-06-27 16:21:51 +0100

    Update Remove-Appx-AllUsers-CloudSourceList.ps1

--- BuiltInApps/Remove-Appx-AllUsers-CloudSourceList.ps1
+++ BuiltInApps/Remove-Appx-AllUsers-CloudSourceList.ps1
@@ -96,7 +96,7 @@ Begin {
 
     # Black List of Appx Provisioned Packages to Remove for All Users
     $BlackListedAppsURL = $null
-    $BlackListedAppsURL = "https://raw.githubusercontent.com/byteben/Windows-11/main/BuiltInApps/blacklist_w11.txt"
+    $BlackListedAppsURL = "https://raw.githubusercontent.com/MSEndpointMgr/Windows/master/BuiltInApps/blacklist_w11.txt"
     Write-LogEntry -Value "BlackListedAppsURL:$($BlackListedAppsURL)"
 
     #Attempt to obtain list of BlackListedApps
